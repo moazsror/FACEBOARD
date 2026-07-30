13af014305c8d4ec44d95d550685db699d17a1ff
moazsror <moazsrorgo@gmail.com>
2026-07-30 16:12:14 +0300

    removed unnecessary buttons and added light mode to local storage

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
-const leftBn = document.getElementById('leftButton');
-const middleBn = document.getElementById('middleButton');
-const rightBn = document.getElementById('rightButton');
 const joinBn = document.getElementById('joinButton');
 const darkBn = document.getElementById('darkButton');
 
+
+if (localStorage.getItem('theme') == 'light')
+  {
+  document.body.classList.toggle('light-mode');
+  darkBn.textContent = 'Enable Dark Mode'
+  }
+
 darkBn.addEventListener('click',()=>{
   document.body.classList.toggle('light-mode');
+
+  if (document.body.classList.contains('light-mode')) {
+
+  localStorage.setItem('theme', 'light');
+  } 
+  else 
+  {
+
+
+  localStorage.setItem('theme', 'dark');
+  }
   if (darkBn.textContent != 'Enable Dark Mode')
     darkBn.textContent = 'Enable Dark Mode'
   else
     darkBn.textContent = 'Disable Dark Mode'
 })
-
-leftBn.addEventListener('click', () => {
-  alert('you clicked the left button');
-});
-
-
-middleBn.addEventListener('click', () =>{
-  if (middleBn.textContent != 'clicked')
-    middleBn.textContent = 'clicked';
-  else
-    middleBn.textContent = 'click';
-});
-
-rightBn.addEventListener('click', ()=>{
-  rightBn.classList.toggle('right-button-clicked');
-
-  if (rightBn.textContent != 'Undo')
-    rightBn.textContent = 'Undo';
-  else
-    rightBn.textContent = 'Change';
-});
-
 
 joinBn.addEventListener('click', ()=>{
   joinBn.classList.toggle('join-button-clicked');
