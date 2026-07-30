3b10567e4bea3e69d915de4b99bf329ac21f97da
moazsror <moazsrorgo@gmail.com>
2026-07-30 20:58:37 +0300

    changed join button to add, added html generation instead of fixed html code, added more profile cards anf fixed some bugs

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-const joinBn = document.getElementById('joinButton');
+
+const profileData = [ {name: "Mark", description: "Mark is a successful influencer since the 80s", img: "images/pp.jpg" },               {name: "Sarah", description: "Sarah is a talented creative designer", img: "images/pp.jpg" },
+                      {name: "John", description: "John loves writing clean back-end code", img: "images/pp.jpg" }
+]
+
 const darkBn = document.getElementById('darkButton');
+const profileSection = document.getElementById('profileContainer');
+
+
+
+  
+profileData.forEach((profile,index)=>{
+  profileSection.innerHTML += `<article id="profileCard${index}" class="profile-card">
+      <img class="profile-picture" src="${profile.img}" alt="">
+      <h3 class="profile-name">${profile.name}</h3>
+      <button class="add-button">Add</button>
+      <p class="profile-description">${profile.description}</p>
+    </article>`
+})
+
+const allAddButtons = document.querySelectorAll('.add-button');
+
+
+
 
 
 if (localStorage.getItem('theme') == 'light')
@@ -17,21 +39,32 @@ darkBn.addEventListener('click',()=>{
   } 
   else 
   {
-
-
   localStorage.setItem('theme', 'dark');
   }
   if (darkBn.textContent != 'Enable Dark Mode')
     darkBn.textContent = 'Enable Dark Mode'
   else
     darkBn.textContent = 'Disable Dark Mode'
-})
-
-joinBn.addEventListener('click', ()=>{
-  joinBn.classList.toggle('join-button-clicked');
-  
-  if (joinBn.textContent != 'Joined')
-    joinBn.textContent = 'Joined';
-  else
-    joinBn.textContent = 'Join';
 });
+
+
+
+
+
+
+
+
+allAddButtons.forEach((button) => {
+  button.addEventListener('click', ()=>{
+  button.classList.toggle('add-button-clicked');
+  
+  if (button.textContent != 'Added')
+    button.textContent = 'Added';
+  else
+    button.textContent = 'Add';
+  }
+);
+});
+
+
+
