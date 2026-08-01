a8962a2920ed4bf849f2436417b14d2268c3c5e1
moazsror <moazsrorgo@gmail.com>
2026-08-01 17:58:31 +0300

    removed add button, added  a function for generating html , added the ability to add more profile cards and save it to local storage

--- app.js
+++ app.js
@@ -1,24 +1,79 @@
-
-const profileData = [ {name: "Mark", description: "Mark is a successful influencer since the 80s", img: "images/pp.jpg" },               {name: "Sarah", description: "Sarah is a talented creative designer", img: "images/pp.jpg" },
+const profileData = JSON.parse(localStorage.getItem('profileDataSave')) || [ {name: "Mark", description: "Mark is a successful influencer since the 80s", img: "images/pp.jpg" },               {name: "Sarah", description: "Sarah is a talented creative designer", img: "images/pp.jpg" },
                       {name: "John", description: "John loves writing clean back-end code", img: "images/pp.jpg" }
 ]
 
+//localStorage.clear();
+
 const darkBn = document.getElementById('darkButton');
 const profileSection = document.getElementById('profileContainer');
+const createUserBtn = document.getElementById('createButton');
+
+const pageHead = document.getElementById('pageHeader');
+
+function generateProfileHTML(){
+  profileSection.innerHTML = '';
+  profileData.forEach((profile,index)=>{
+    profileSection.innerHTML += `<article id="profileCard${index}" class="profile-card">
+        <img class="profile-picture" src="${profile.img}" alt="">
+        <h3 class="profile-name">${profile.name}</h3>
+        <p class="profile-description">${profile.description}</p>
+      </article>`;
+
+});
+}
+
+function saveProfileDataToLocalStorage(){
+  localStorage.setItem('profileDataSave', JSON.stringify(profileData))
+}
 
 
 
-  
-profileData.forEach((profile,index)=>{
-  profileSection.innerHTML += `<article id="profileCard${index}" class="profile-card">
-      <img class="profile-picture" src="${profile.img}" alt="">
-      <h3 class="profile-name">${profile.name}</h3>
-      <button class="add-button">Add</button>
-      <p class="profile-description">${profile.description}</p>
-    </article>`
+generateProfileHTML();
+
+
+
+
+
+
+createUserBtn.addEventListener(('click'), ()=>{
+  pageHead.innerHTML += `<form id="userForm" class="user-form">
+        <input id='nameInput' type="text" placeholder="Name">
+        <input id='descInput' type="text" placeholder="Description">
+        <button type="button" id="submitUserButton" class="create-button">Create Card</button>
+      </form>`
+      const submitUserBtn = document.getElementById('submitUserButton');
+
+      submitUserBtn.addEventListener(('click'), ()=>{
+        const nameIn = document.getElementById('nameInput').value;
+        const descIn = document.getElementById('descInput').value;
+        if (nameIn != '' && descIn != ''){
+          profileData.push({name: nameIn, description: descIn, img: "images/pp.jpg"});
+          pageHead.innerHTML = `<button id="darkButton" class="dark-button">
+                Disable Dark Mode
+              </button>
+              <button id="createButton" class="dark-button">
+                Create User Card
+              </button>`
+          generateProfileHTML();
+          saveProfileDataToLocalStorage();
+          window.location.reload();
+        }
+        else {
+          pageHead.innerHTML = `<button id="darkButton" class="dark-button">
+                Disable Dark Mode
+              </button>
+              <button id="createButton" class="dark-button">
+                Create User Card
+              </button>`
+          generateProfileHTML();
+          window.location.reload();
+        }
+})
 })
 
-const allAddButtons = document.querySelectorAll('.add-button');
+
+
+
 
 
 
@@ -54,17 +109,8 @@ darkBn.addEventListener('click',()=>{
 
 
 
-allAddButtons.forEach((button) => {
-  button.addEventListener('click', ()=>{
-  button.classList.toggle('add-button-clicked');
-  
-  if (button.textContent != 'Added')
-    button.textContent = 'Added';
-  else
-    button.textContent = 'Add';
-  }
-);
-});
+
+
 
 
 
